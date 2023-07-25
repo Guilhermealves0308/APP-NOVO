fc4b46aab114504c9de296128f8a2e3b6bab5d21
guilherme <gilhermeglegrorio67@gmail.com>
2023-07-25 13:03:33 -0300

    ta indo

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -9,11 +9,11 @@ const routes: Routes = [
     children: [
       {
         path: 'tab1',
-        loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+        loadChildren: () => import('../pages/home/home.module').then(m => m.HomePageModule)
       },
       {
         path: 'tab2',
-        loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
+        loadChildren: () => import('../pages/agendar/agendar.module').then(m => m.AgendarPageModule)
       },
       {
         path: 'tab3',
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { OptionsModalComponent } from '../Componentes/options-modal/options-modal.component';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
-  styleUrls: ['tabs.page.scss']
+  styleUrls: ['tabs.page.scss'],
 })
 export class TabsPage {
 
-  constructor() {}
+  constructor(private modalController: ModalController) {}
 
+  async openOptionsModal() {
+    const modal = await this.modalController.create({
+      component: OptionsModalComponent,
+      cssClass: 'my-custom-modal-css', // Estilo personalizado, se necessário
+    });
+
+    await modal.present();
+  }
 }
