ec66dece4e0316a667b1e334a6b87fa5e2afcbdd
guilherme <gilhermeglegrorio67@gmail.com>
2023-07-26 15:51:08 -0300

    Opções Pronta, faltar as rotas

--- src/app/pages/opcoes/opcoes.page.ts
+++ src/app/pages/opcoes/opcoes.page.ts
@@ -6,10 +6,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./opcoes.page.scss'],
 })
 export class OpcoesPage implements OnInit {
-
+  darkMode: boolean = true;
+  lightMode: boolean = false;
+  
   constructor() { }
 
   ngOnInit() {
+  }
+
+
+  toggleDarkMode() {
+    // Implement logic to toggle dark mode here
+    if (this.darkMode) {
+      // Code to enable dark mode
+      this.enableDarkMode();
+    } else {
+      // Code to disable dark mode
+      this.disableDarkMode();
+    }
+  }
+
+  toggleLightMode() {
+    // Implement logic to toggle light mode here
+    if (this.lightMode) {
+      // Code to enable light mode
+      this.enableLightMode();
+    } else {
+      // Code to disable light mode
+      this.disableLightMode();
+    }
+  }
+
+  private enableDarkMode() {
+    // Code to enable dark mode
+    document.body.classList.add('dark-theme');
+  }
+
+  private disableDarkMode() {
+    // Code to disable dark mode
+    document.body.classList.remove('dark-theme');
+  }
+
+  private enableLightMode() {
+    // Code to enable light mode
+    document.body.classList.add('light-theme');
+  }
+
+  private disableLightMode() {
+    // Code to disable light mode
+    document.body.classList.remove('light-theme');
   }
 
 }
